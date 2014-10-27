328e3b838f6fec69ac85ec9be0455656ee1ce911
Jayanta <Jayanta@Bunty>
2014-10-27 13:03:19 +0530

    Embeding HTML Editor For Templates, Added Extension tinyMCE

--- cma/protected/views/templates/_form.php
+++ cma/protected/views/templates/_form.php
@@ -21,9 +21,27 @@
 		<?php echo $form->error($model,'template_name'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'template_html'); ?>
-		<?php echo $form->textField($model,'template_html',array('size'=>60,'maxlength'=>2000)); ?>
+	<div class="tinymce">
+		<?php echo $form->labelEx($model,'contractData'); ?><br />
+		<?php $this->widget('application.extensions.tinymce.ETinyMce',
+		array(
+		'model'=>$model,
+		'attribute'=>'template_html',
+		'editorTemplate'=>'full',
+		'htmlOptions'=>array('rows'=>6, 'cols'=>50, 'class'=>'tinymce'),
+		'options' => array(
+			'theme_advanced_buttons1' =>
+			'undo,redo,|,bold,italic,underline,|,justifyleft,justifycenter,justifyright,justifyfull,|,outdent, indent,|,advhr,|,sub,sup,|,bullist,numlist,|,formatselect,fontselect,fontsizeselect,|,cut,copy,paste,pastetext,pasteword,|,search,replace,',
+		'theme_advanced_buttons2' => 'tablecontrols,|,removeformat,visualaid,',
+		'theme_advanced_buttons3' => '',
+		'theme_advanced_buttons4' => '',
+		'theme_advanced_toolbar_location' => 'top',
+		'theme_advanced_toolbar_align' => 'left',
+		'theme_advanced_statusbar_location' => 'none',
+		'theme_advanced_font_sizes' => "10=10pt,11=11pt,12=12pt,13=13pt,14=14pt,
+		15=15pt,16=16pt,17=17pt,18=18pt,19=19pt,20=20pt",
+		)
+		)); ?>
 		<?php echo $form->error($model,'template_html'); ?>
 	</div>
 
